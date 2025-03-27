7b871e0a48ea1598b32e338c24c5a858f39f04d2
Ammo Dulacha <s4044699@student.rmit.edu.au>
2025-03-28 04:27:17 +1100

    Updated size content and fixed bugs

--- a1/js/script.js
+++ a1/js/script.js
@@ -1,1 +1,8 @@
-ECHO is on.
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdown = document.querySelector(".dropdown-menu");
+
+    dropdown.addEventListener("change", function (event) {
+        window.location.href = event.target.value;
+    });
+});
+
